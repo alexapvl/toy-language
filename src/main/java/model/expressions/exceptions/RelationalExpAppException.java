e43feebe780d9b8d3019092fa97b71844351de8e
alexapvl <alex.robite@gmail.com>
2024-11-12 19:35:14 +0200

    feat: new relational exception

---
+++ src/main/java/model/expressions/exceptions/RelationalExpAppException.java
@@ -0,0 +1,10 @@
+package model.expressions.exceptions;
+
+import model.exceptions.AppException;
+
+public class RelationalExpAppException extends AppException {
+  public RelationalExpAppException(String message) {
+    super(message);
+  }
+
+}
