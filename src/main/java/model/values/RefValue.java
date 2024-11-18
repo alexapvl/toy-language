f220f38505ef76f39fa1cb22c47db28e97717816
alexapvl <alex.robite@gmail.com>
2024-11-18 17:34:49 +0200

    feat: added RefValue

---
+++ src/main/java/model/values/RefValue.java
@@ -0,0 +1,47 @@
+package model.values;
+
+import model.types.IType;
+import model.types.RefType;
+
+public class RefValue implements IValue {
+  private int address;
+  private IType locationType;
+
+  public RefValue(int address, IType locationType) {
+    this.address = address;
+    this.locationType = locationType;
+  }
+
+  public int getAddr() {
+    return this.address;
+  }
+
+  public IType getLocationType() {
+    return this.locationType;
+  }
+
+  @Override
+  public boolean equals(IValue obj) {
+    if (obj instanceof RefValue) {
+      return this.address == ((RefValue) obj).getAddr()
+          && this.locationType.equals(((RefValue) obj).getLocationType());
+    }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return "(" + this.address + ", " + this.locationType + ")";
+  }
+
+  @Override
+  public IType getType() {
+    return new RefType(this.locationType);
+  }
+
+  @Override
+  public IValue deepCopy() {
+    return new RefValue(this.address, this.locationType.deepCopy());
+  }
+
+}
