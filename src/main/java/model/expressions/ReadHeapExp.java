54ee390f26a2855956a03ca6f68977e1ded41f63
alexapvl <alex.robite@gmail.com>
2024-11-19 09:30:12 +0200

    feat: read from heap expression

---
+++ src/main/java/model/expressions/ReadHeapExp.java
@@ -0,0 +1,43 @@
+package model.expressions;
+
+import model.adt.dictionary.IGenericDictionary;
+import model.adt.heap.IGenericHeap;
+import model.exceptions.AppException;
+import model.values.IValue;
+import model.values.RefValue;
+
+public class ReadHeapExp implements IExp {
+  private IExp exp;
+
+  public ReadHeapExp(IExp exp) {
+    this.exp = exp;
+  }
+
+  @Override
+  public IValue eval(IGenericDictionary<String, IValue> symTable, IGenericHeap<Integer, IValue> heap)
+      throws AppException, ArithmeticException {
+
+    IValue value = this.exp.eval(symTable, heap);
+    if (!(value instanceof RefValue)) {
+      throw new AppException("The expression does not evaluate to a RefValue");
+    }
+
+    RefValue refValue = (RefValue) value;
+    Integer address = refValue.getAddr();
+    if (!heap.contains(address)) {
+      throw new AppException("The address " + address + " is not in the heap (it was not allocated)");
+    }
+
+    return heap.lookup(address);
+  }
+
+  @Override
+  public IExp deepCopy() {
+    return new ReadHeapExp(this.exp.deepCopy());
+  }
+
+  @Override
+  public String toString() {
+    return "readHeap(" + this.exp + ")";
+  }
+}
