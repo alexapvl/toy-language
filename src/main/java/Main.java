14738f90e65d7ec47fd01cee3308d01fc279ed34
alexapvl <alex.robite@gmail.com>
2025-01-06 21:47:34 +0200

    feat: java fx works, check Main.java to see command to run for program to start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,33 @@
-import view.Interpreter;
+// import view.Interpreter;
 
-public class Main {
+// public class Main {
+//     public static void main(String[] args) {
+//         Interpreter.main(args);
+//     }
+// }
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+// run command: mvn clean javafx:run
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        Interpreter.main(args);
+        launch(args);
     }
 }
