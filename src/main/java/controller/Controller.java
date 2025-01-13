1e1d2c16773570469e007dd3a6af9020c8ea1232
alexapvl <alex.robite@gmail.com>
2025-01-13 15:41:35 +0200

    feat: changed the constructor to initialize correctly the Controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -26,6 +26,7 @@ public class Controller {
   public Controller(IRepository repo, boolean displayFlag) {
     this.repo = repo;
     this.displayFlag = displayFlag;
+    this.executor = Executors.newFixedThreadPool(2);
   }
 
   public boolean getDisplayFlag() {
